47487a779112d7e66dc94cbe70fed06b15982f86
scheidm <scheidm@wellcomposed.net>
2015-04-14 08:14:18 -0400

    migration of old data to yaml, progress to replace handlebars calls with straight ruby/haml

--- tilt.rb
+++ tilt.rb
@@ -1,24 +1,22 @@
+require 'yaml'
+require 'tilt'
+
 s=ARGV[0]
 filename=s.split('.')[0]
 structure=filename.split('/')
 changed=structure[-1]
-require 'tilt'
 print s
 print changed
  
-sites = {
-    "Nettuts+" => "http://net.tutsplus.com", 
-    "Psdtuts+" => "http://psd.tutsplus.com",
-    "Mobiletuts+" => "http://mobile.tutplus.com",
-    "Audiotuts+"  => "http://audio.tutsplus.com" }
- 
+#template = Tilt::HamlTemplate.new("source/layouts/default.html.haml")
 template = Tilt::HamlTemplate.new("source/layout.html.haml")
+site = YAML::load_file('site.yaml')
 context = Object.new
 def context.title
     "Mark Scheid"
 end
 File.open( "compiled/pages/#{changed}.html", "w") do |file|
-    file.write template.render(context) {
-        Tilt::HamlTemplate.new("#{s}").render(Object.new, sites: sites)
+    file.write template.render(context, data: site) {
+        Tilt::HamlTemplate.new("#{s}").render(context, data: site)
     }
 end
