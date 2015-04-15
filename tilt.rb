436a6c234b4b2cd4c85199a6f2543fb4ae056634
scheidm <scheidm@wellcomposed.net>
2015-04-14 21:02:58 -0400

    migration of compiled assets complete, must move sass and local css but we are cooking with GAS

--- tilt.rb
+++ tilt.rb
@@ -6,16 +6,15 @@ filename=s.split('.')[0]
 structure=filename.split('/')
 changed=structure[-1]
 print s
-print changed
  
-#template = Tilt::HamlTemplate.new("source/layouts/default.html.haml")
-template = Tilt::HamlTemplate.new("source/layout.html.haml")
+template = Tilt::HamlTemplate.new("source/layouts/default.html.haml")
+#template = Tilt::HamlTemplate.new("source/layout.html.haml")
 site = YAML::load_file('site.yaml')
 context = Object.new
 def context.title
     "Mark Scheid"
 end
-File.open( "compiled/pages/#{changed}.html", "w") do |file|
+File.open( "compiled/#{changed}.html", "w") do |file|
     file.write template.render(context, data: site) {
         Tilt::HamlTemplate.new("#{s}").render(context, data: site)
     }
